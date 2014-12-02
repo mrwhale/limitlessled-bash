010bb9e7a6b5292a6f3a6511b6655d18391b9fe8
mrwhale <harrysibenaler@gmail.com>
2014-12-02 20:11:23 +1100

    basic functions working - can turn on/off white and rgbw lights, added more to readme

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 limitlessled-bash
 =================
 
-command line control over limitlessLED lights
+Command line control over limitlessLED lights
 
 I trolled the internet and only found snippets of stuff for limitless and command line linux,
 so I decided to create a shell script myself. Primarily to run off my raspberry pi, but in theory can be used anywhere
 
 I'm lazy, and this is my first commit, so my coding practices/standards are all over the shop. Once the functionality is there I will clean everything up :)
+
+This works with v3+ of the wifi bridge (when they changed it to port 8899 instead of 5000)
+
+basic command is in this format: ./led.sh [type] [zone] [command] [param]
+./led.sh w 1 on
+# This will turn on zone 1 white lights
+
+./led.sh c 0 off
+# This will turn ALL RGBW lights off (I use 0 for global)
+
+./led.sh w 1 b up
+# This will turn the brightness up 1 for zone 1 white lights
+
+
+All possible commands
+./led.sh [w,c] [0..4] [on,off,c,b] [up,down,1..10,red,blue,green,yellow,purple]
+
+I will add references and thanks in when I can find the websites I got inspiration from :)
--- led.sh
+++ led.sh
@@ -1,11 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 
-if [ -z "$1" ] ; then 
-    echo "You must enter a parameter: "  
-    echo "  e.g. .\led.sh c 1 on #turns colour zone 1 one "
-    exit 1 
-fi 
-
+if [ -z $1 ]
+then
+    echo "You must enter a parameter:"
+    echo "e.g. 'led.sh' c 1 on #turns colour zone 1 one"
+    exit "1"
+fi
 
 type="$1"
 zone="$2"
@@ -13,38 +13,61 @@ command="$3"
 param="$4"
 
 function colour {
-	ipaddress="10.1.1.23" 
+	ipaddress="10.1.1.23"
 	portnum="8899"
 	#Colour Commands
-	allcon="\x35\00\x55" 
-	allcoff="\x39\00\x55" 
-	zone1con="\x38\00\x55" 
-	zone1coff="\x3B\00\x55" 
-	zone2con="\x3D\00\x55" 
-	zone2coff="\x33\00\x55" 
-	zone3con="\x37\00\x55" 
-	zone3coff="\x3A\00\x55" 
-	zone4con="\x32\00\x55" 
-	zone4coff="\x36\00\x55"
-	#TODO add brightness and colour commands
-	
-	
+	on0="\x42\00\x55"
+	off0="\x41\00\x55"
+	on1="\x45\00\x55"
+	off1="\x46\00\x55"
+	on2="\x47\00\x55"
+	off2="\x48\00\x55"
+	on3="\x49\00\x55"
+	off3="\x4A\00\x55"
+	on4="\x4B\00\x55"
+	off4="\x4C\00\x55"
+	#TODO add brightness and colour command
+
+        zone="$1"
+        command="$2"
+        param="$3"
+        if [ $command = "b" ] || [ $command = "B" ]
+        then
+                echo "brightness"
+	elif [ $command = "c" ] || [ $command = "C" ]
+	then
+		echo "color"
+        elif [ $command = "on" ] || [ $command = "ON" ]
+        then
+                echo "on"
+                cmd=on$zone
+                eval cmd=\$$cmd
+                echo $cmd
+                echo -n -e "$cmd" >/dev/udp/$ipaddress/$portnum
+        elif [ $command = "off" ] || [ $command = "OFF" ]
+        then
+                echo "off"
+                cmd=off$zone
+                eval cmd=\$$cmd
+                echo $cmd
+                echo -n -e "$cmd" >/dev/udp/$ipaddress/$portnum
+        fi
 }
 
 function white {
-	ipaddress="10.1.1.23" 
+	ipaddress="10.1.1.23"
 	portnum="8899"
 	#white commands
-	on0="\x35\00\x55" 
-	off0="\x39\00\x55" 
-	on1="\x38\00\x55" 
-	off1="\x3B\00\x55" 
-	on2="\x3D\00\x55" 
-	off2="\x33\00\x55" 
-	on3="\x37\00\x55" 
-	off3="\x3A\00\x55" 
-	on4="\x32\00\x55" 
-	off4="\x36\00\x55" 
+	on0="\x35\00\x55"
+	off0="\x39\00\x55"
+	on1="\x38\00\x55"
+	off1="\x3B\00\x55"
+	on2="\x3D\00\x55"
+	off2="\x33\00\x55"
+	on3="\x37\00\x55"
+	off3="\x3A\00\x55"
+	on4="\x32\00\x55"
+	off4="\x36\00\x55"
 	#TODO add brightness commands for white
 	zone="$1"
 	command="$2"
@@ -54,28 +77,27 @@ function white {
 		echo "brightness"
 	elif [ $command = "on" ] || [ $command = "ON" ]
 	then
-		echo "on"
+		#echo "on"
 		cmd=on$zone
-		echo $cmd
 		eval cmd=\$$cmd
-		echo -n -e "$cmd" >/dev/udp/"$ipaddress"/"$portnum"
+                #echo $cmd
+		echo -n -e "$cmd" >/dev/udp/$ipaddress/$portnum
 	elif [ $command = "off" ] || [ $command = "OFF" ]
 	then
-		echo "off"
+		#echo "off"
 		cmd=off$zone
-		echo $cmd
 		eval cmd=\$$cmd
-		echo -n -e "$cmd" >/dev/udp/"$ipaddress"/"$portnum"
-	fi	
-	
+		#echo $cmd
+		echo -n -e "$cmd" >/dev/udp/$ipaddress/$portnum
+	fi
 }
 
-if [ $type = "c"] || [ $type = "C" ]
+if [ $type = "c" ] || [ $type = "C" ]
 then
 	echo "you want to turn colour lights"
 	colour $zone $command $param	
 elif [ $type = "w" ] || [ $type = "W" ]
 then
-	echo "you want to turn white"
+	#echo "you want to turn white"
 	white $zone $command $param
 fi
