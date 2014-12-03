5a90db5fc67dbe54392a1ae51549a5c7bc063355
mrwhale <harrysibenaler@gmail.com>
2014-12-03 14:01:48 +1100

    Added ability to change colours, only have main colours for now. Also cleaned up variables

--- led.sh
+++ led.sh
@@ -13,30 +13,24 @@ command="$3"
 param="$4"
 ipaddress="10.1.1.23"
 portnum="8899"
+declare -A colours=( ["purple"]="\x40\xF0\x55" ["blue"]="\x40\x10\x55" ["red"]="\x40\xb0\x55" ["green"]="\x40\x60\x55" ["yellow"]="\x40\x80\x55" ["pink"]="\x40\xC0\x55" ["orange"]="\x40\xA0\x55" )
 
 function colour {
-	#Colour Commands
+	#RGBW bulb Commands
 	onarray=("\x42\00\x55" "\x45\00\x55" "\x47\00\x55" "\x49\00\x55" "\x4B\00\x55")
 	offarray=("\x41\00\x55" "\x46\00\x55" "\x48\00\x55" "\x4A\00\x55" "\x4C\00\x55")
-	purple="\x40\xFF\x55"
+	#purple="\x40\xFF\x55"
 	#TODO add brightness and colour command
 	whitearray=("\xC2\00\x55" "\xC5\00\x55" "\xC7\00\x55" "\xC9\00\x55" "\xC9\00\x55")
-
         if [ $command = "b" ] || [ $command = "B" ]
         then
                 echo "brightness"
 	elif [ $command = "c" ] || [ $command = "C" ]
 	then
 		echo "You just changed colour zone $zone to $param"
-		cmd=on$zone
-		cmd2=$param
-		eval cmd=\$$cmd
-		eval cmd2=\$$cmd2
-		echo $cmd
-		echo $cmd2
-		echo -n -e "$cmd" >/dev/udp/$ipaddress/$portnum
+		echo -n -e "${onarray[$zone]}" >/dev/udp/$ipaddress/$portnum
 		sleep 0.01
-		echo -n -e "$cmd2" >/dev/udp/$ipaddress/$portnum
+		echo -n -e "${colours[$param]}" >/dev/udp/$ipaddress/$portnum
         elif [ $command = "on" ] || [ $command = "ON" ]
         then
                 echo "You just turned colour zone $zone on" 
