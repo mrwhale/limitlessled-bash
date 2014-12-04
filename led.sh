55d0c1d7b84a1c7addfb5f9fe1ff291345c0fdbb
mrwhale <harrysibenaler@gmail.com>
2014-12-04 14:14:07 +1100

    Tweeked the colours, made purple more purple and blue more blue, previoously they looked to alike

--- led.sh
+++ led.sh
@@ -17,7 +17,7 @@ command="$3"
 param="$4"
 
 #Colour array
-declare -A colours=( ["purple"]="\x40\xF0\x55" ["blue"]="\x40\x10\x55" ["red"]="\x40\xb0\x55" ["green"]="\x40\x60\x55" ["yellow"]="\x40\x80\x55" ["pink"]="\x40\xC0\x55" ["orange"]="\x40\xA0\x55" )
+declare -A colours=( ["purple"]="\x40\x00\x55" ["blue"]="\x40\x20\x55" ["red"]="\x40\xb0\x55" ["green"]="\x40\x60\x55" ["yellow"]="\x40\x80\x55" ["pink"]="\x40\xC0\x55" ["orange"]="\x40\xA0\x55" )
 
 function colour {
 	#RGBW bulb Commands
