3f5c00ad75672962732e187cde19b823e8dd6600
Chris Prescott <cmprescott@gmail.com>
2014-12-05 00:44:46 -0500

    More style changes. To do break this commit apart.

--- led.sh
+++ led.sh
@@ -24,22 +24,17 @@ command="$3"
 param="$4"
 
 ##########
-# Global constants
-##########
-#Constant bits
-ctrl="\x55"
-standby="\00"
-
-##########
 # Helper functions
 ##########
 # Generic send any command the controller
 function sendCmd {
-	cmd=$1
+	ctrl="\x55"
+    cmd=$1
 	echo -n -e "$cmd$ctrl" >/dev/udp/$ipaddress/$portnum
 }
 # Select zone by sending standby cmd and sleep for a second
 function selectZone {
+    standby="\00"
 	sendCmd "${onarray[$zone]}$standby"
 	sleep 0.01
 }
@@ -48,47 +43,69 @@ function selectZone {
 # lightbulb type specific functions
 ##########
 function colour {
-	#RGBW bulb Commands
+	## RGBW bulb variables and commands
 	onarray=("\x42" "\x45" "\x47" "\x49" "\x4B")
-	offarray=("\x41" "\x46" "\x48" "\x4A" "\x4C")
+    function sendOnCommand {
+        standby="\00"
+        sendCmd "${onarray[$zone]}$standby"
+    }
     
-    # Brightness	
+	offarray=("\x41" "\x46" "\x48" "\x4A" "\x4C")
+    function sendOffCommand {
+        standby="\00"
+        sendCmd "${offarray[$zone]}$standby"
+    }
+    
+    brightarray=("\x02" "\x04" "\x08" "\x0A" "\x0B" "\xD0" "\x10" "\x13" "\x16" "\x19" "\x1B")
+    function sendBrightCmd {    
+        selectZone
+        bright="\x4E"
+        cmd=$1
+        sendCmd "$bright$cmd"
+    }    
+    
+    declare -A colours=( ["purple"]="\x00" ["blue"]="\x20" ["red"]="\xb0" ["green"]="\x60" ["yellow"]="\x80" ["pink"]="\xC0" ["orange"]="\xA0" )
+    function sendColorCmd {
+        selectZone
+        color="\x40"
+        cmd=$1
+        sendCmd "$color$cmd"
+    }
+    
+    whitearray=("\xC2" "\xC5" "\xC7" "\xC9" "\xC9")
+    function sendWhiteCmd {
+        selectZone
+        white="\00"
+        cmd=$1
+        sendCmd "$cmd$white"        
+    }
+    
+    ## Input handling
+    # Brightness
 	if [ $command = "b" ] || [ $command = "B" ]
 	then
-        # Constants
-        bright="\x4E"    
-        brightarray=("\x02" "\x04" "\x08" "\x0A" "\x0B" "\xD0" "\x10" "\x13" "\x16" "\x19" "\x1B")
-        
 		if [ $param = "full" ]
 		then
-			cmd="$bright\x3B"
 			echo "You turned colour bulbs in zone $zone to full brightness"
-			selectZone
-			sendCmd "$cmd"
+			sendBrightCmd "${brightarray[18]}"
 		elif [ $param -ge 0 -a $param -le 10 ]
 		then
 			echo "You turned colour bulbs in zone $zone to $param"
-			selectZone
-			sendCmd "$bright${brightarray[$param]}"
+			sendBrightCmd "${brightarray[$param]}"
 		else
 			echo "You've done something wrong"
 	fi
     # Color
 	elif [ $command = "c" ] || [ $command = "C" ]
     then
-        color="\x40"
-        whitearray=("\xC2\00" "\xC5\00" "\xC7\00" "\xC9\00" "\xC9\00")
-        declare -A colours=( ["purple"]="\x00" ["blue"]="\x20" ["red"]="\xb0" ["green"]="\x60" ["yellow"]="\x80" ["pink"]="\xC0" ["orange"]="\xA0" )
-
 		# Check to make sure that the colour specified in the array before trying
 		isin=1
 		if [ $param = "white" ]
 		then
 			echo "You just turned colour bulbs in zone $zone back to white"
-            selectZone
-			sendCmd "${whitearray[$zone]}"
+			sendWhiteCmd "${whitearray[$zone]}"
 		else
-			for i in "$color${!colours[@]}"
+			for i in "${!colours[@]}"
 			do
             	if [ "$i" = "$param" ]
                 then
@@ -99,8 +116,7 @@ function colour {
 			if [ "$isin" -eq "0" ]
 			then
 				echo "You just changed colour bulbs in zone $zone to $param"
-				selectZone
-				sendCmd "$color${colours[$param]}"
+				sendColorCmd "${colours[$param]}"
 			else
 				echo "Colour $param isn't configured"
 			fi
@@ -109,12 +125,12 @@ function colour {
 		elif [ $command = "on" ] || [ $command = "ON" ]
 		then
 			echo "You just turned colour bulbs in zone $zone on" 
-			sendCmd "${onarray[$zone]}$standby"
+			sendOnCommand
         # Off
 		elif [ $command = "off" ] || [ $command = "OFF" ]
 		then
 			echo "You just turned colour bulbs in zone $zone off"
-			sendCmd "${offarray[$zone]}$standby"
+			sendOffCommand
 	else
 		echo "You've done something wrong"
 	fi
@@ -206,10 +222,12 @@ function white {
 	elif [ $command = "on" ] || [ $command = "ON" ]
 	then
 		echo "You just turned white bulbs in zone $zone on"
+        standby="\00"
 		sendCmd "${onarray[$zone]}$standby"
 	elif [ $command = "off" ] || [ $command = "OFF" ]
 	then
 		echo "You just turned white bulbs in zone $zone off"
+        standby="\00"
 		sendCmd "${offarray[$zone]}$standby"
 	else
 		echo "You've done something wrong"
