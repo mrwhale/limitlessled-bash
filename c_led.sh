0583b8bb1523284be6c7adc49c87acef756f619e
Chris Prescott <cmprescott@gmail.com>
2014-12-06 18:11:54 -0500

    Style change to make tabbing more consistent with just 4 spaces.

--- c_led.sh
+++ c_led.sh
@@ -2,9 +2,9 @@
 
 if [ -z $1 ]
 then
-        echo "You must enter a parameter:"
-        echo "e.g. 'led.sh' 1 on #turns colour zone 1 on"
-        exit "1"
+    echo "You must enter a parameter:"
+    echo "e.g. 'led.sh' 1 on #turns colour zone 1 on"
+    exit "1"
 fi
 
 ##########
@@ -26,68 +26,68 @@ param="$3"
 # Send Command Functions
 ##########
 function sendCmd {      # Generic send any command the controller
-        ctrl="\x55"
-        cmd=$1
-        echo -n -e "$cmd$ctrl" | nc -w 1 -u $ipaddress $portnum
+    ctrl="\x55"
+    cmd=$1
+    echo -n -e "$cmd$ctrl" | nc -w 1 -u $ipaddress $portnum
 }
 function sendOnCommand {        # On command is also used to select zones
-        onarray=("\x42" "\x45" "\x47" "\x49" "\x4B")
-        standby="\00"
-        sendCmd "${onarray[$zone]}$standby"
+    onarray=("\x42" "\x45" "\x47" "\x49" "\x4B")
+    standby="\00"
+    sendCmd "${onarray[$zone]}$standby"
 }
 function selectZone {   # Select zone by sending standby cmd and sleep for a second
-        sendOnCommand
-        sleep 0
+    sendOnCommand
+    sleep 0
 }
 function sendOffCommand {
-        offarray=("\x41" "\x46" "\x48" "\x4A" "\x4C")
-        standby="\00"
-        sendCmd "${offarray[$zone]}$standby"
+    offarray=("\x41" "\x46" "\x48" "\x4A" "\x4C")
+    standby="\00"
+    sendCmd "${offarray[$zone]}$standby"
 }
 function sendBrightCmd {
-        brightarray=("\x02" "\x04" "\x08" "\x0A" "\x0B" "\xD0" "\x10" "\x13" "\x16" "\x19" "\x1B")                                      
-        selectZone
-        bright="\x4E"
-        cmd="${brightarray[$1]}"
-        sendCmd "$bright$cmd"
+    brightarray=("\x02" "\x04" "\x08" "\x0A" "\x0B" "\xD0" "\x10" "\x13" "\x16" "\x19" "\x1B")                                      
+    selectZone
+    bright="\x4E"
+    cmd="${brightarray[$1]}"
+    sendCmd "$bright$cmd"
 }
 function sendColorCmd {
-        selectZone
-        color="\x40"
-        cmd=$1
-        sendCmd "$color$cmd"
+    selectZone
+    color="\x40"
+    cmd=$1
+    sendCmd "$color$cmd"
 }
 function sendWhiteCmd {
-        whitearray=("\xC2" "\xC5" "\xC7" "\xC9" "\xC9")
-        selectZone
-        white="\00"
-        cmd="${whitearray[$zone]}"
-        sendCmd "$cmd$white"        
+    whitearray=("\xC2" "\xC5" "\xC7" "\xC9" "\xC9")
+    selectZone
+    white="\00"
+    cmd="${whitearray[$zone]}"
+    sendCmd "$cmd$white"
 }       
 
 ##########
 # Input Handling Functions
 ##########
 function handleOn {
-        echo "You just turned colour bulbs in zone $zone on" 
-        sendOnCommand
+    echo "You just turned colour bulbs in zone $zone on" 
+    sendOnCommand
 }
 function handleOff {
-        echo "You just turned colour bulbs in zone $zone off"
-        sendOffCommand
+    echo "You just turned colour bulbs in zone $zone off"
+    sendOffCommand
 }
 function handleBrightness {
-        if [ $param = "full" ]
-        then
-                echo "You turned colour bulbs in zone $zone to full brightness"
-                sendBrightCmd "18"
-        elif [ $param -ge 0 -a $param -le 10 ]
-        then
-                echo "You turned colour bulbs in zone $zone to $param"
-                sendBrightCmd "$param"
-        else
-                echo "You've done something wrong"
-        fi      
+    if [ $param = "full" ]
+    then
+        echo "You turned colour bulbs in zone $zone to full brightness"
+        sendBrightCmd "18"
+    elif [ $param -ge 0 -a $param -le 10 ]
+    then
+        echo "You turned colour bulbs in zone $zone to $param"
+        sendBrightCmd "$param"
+    else
+        echo "You've done something wrong"
+    fi      
 }
 function handleColor {
     echo "Attempting to change colour bulbs in zone $zone back to $param"
@@ -125,18 +125,18 @@ function handleColor {
 ##########
 if [ $command = "on" ] || [ $command = "ON" ]
 then
-        handleOn
+    handleOn
 elif [ $command = "off" ] || [ $command = "OFF" ]
 then
-        handleOff
+    handleOff
 # Brightness
 elif [ $command = "b" ] || [ $command = "B" ]
 then
-        handleBrightness
+    handleBrightness
 # Color
 elif [ $command = "c" ] || [ $command = "C" ]
 then
-        handleColor
+    handleColor
 else
-        echo "You've done something wrong"
+    echo "You've done something wrong"
 fi
