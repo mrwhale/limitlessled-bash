3a61ce7294999fb13b919c54f613d6a2cf933fd7
Chris Prescott <cmprescott@gmail.com>
2014-12-07 01:04:22 -0500

    Convert tabs to spaces.

--- w_led.sh
+++ w_led.sh
@@ -2,9 +2,9 @@
 
 if [ -z $1 ]
 then
-	echo "You must enter a parameter:"
-	echo "e.g. 'led.sh' 1 on #turns white zone 1 on"
-	exit "1"
+    echo "You must enter a parameter:"
+    echo "e.g. 'led.sh' 1 on #turns white zone 1 on"
+    exit "1"
 fi
 
 ##########
@@ -26,55 +26,55 @@ param="$3"
 # Send Command Functions
 ##########
 function sendCmd { # Generic send any command the controller
-	ctrl="\x55"
+    ctrl="\x55"
     cmd=$1
-	echo -n -e "$cmd$ctrl" >/dev/udp/$ipaddress/$portnum
+    echo -n -e "$cmd$ctrl" >/dev/udp/$ipaddress/$portnum
 }
-function sendOnCommand {	# On command is also used to select zones
-	onarray=("\x35" "\x38" "\x3D" "\x37" "\x32")
-	standby="\00"
-	sendCmd "${onarray[$zone]}$standby"
+function sendOnCommand {    # On command is also used to select zones
+    onarray=("\x35" "\x38" "\x3D" "\x37" "\x32")
+    standby="\00"
+    sendCmd "${onarray[$zone]}$standby"
 }
-function selectZone {	# Select zone by sending standby cmd and sleep for a second
-	sendOnCommand
-	sleep 0.01
+function selectZone {    # Select zone by sending standby cmd and sleep for a second
+    sendOnCommand
+    sleep 0.01
 }
 function sendOffCommand {
-	offarray=("\x39" "\x3B" "\x33" "\x3A" "\x36")
-	standby="\00"
-	sendCmd "${offarray[$zone]}$standby"
+    offarray=("\x39" "\x3B" "\x33" "\x3A" "\x36")
+    standby="\00"
+    sendCmd "${offarray[$zone]}$standby"
 }
 function sendNightCommand {
-	nightarray=("\xB9\00" "\xBB\00" "\xB3\00" "\xBA\00" "\xB6\00")	
-	selectZone
-	sendCmd "${nightarray[$zone]}"	
+    nightarray=("\xB9\00" "\xBB\00" "\xB3\00" "\xBA\00" "\xB6\00")    
+    selectZone
+    sendCmd "${nightarray[$zone]}"    
 }
 function sendFullBrightCommand {
-	fullbrightarray=("\xB5\00" "\xB8\00" "\xBD\00" "\xB7\00" "\xB2\00")
-	selectZone
-	sendCmd "${fullbrightarray[$zone]}"
+    fullbrightarray=("\xB5\00" "\xB8\00" "\xBD\00" "\xB7\00" "\xB2\00")
+    selectZone
+    sendCmd "${fullbrightarray[$zone]}"
 }
 function sendBrightDimCommand {
-	brightDim=$1
-	selectZone
-	sendCmd "$brightDim\00"
+    brightDim=$1
+    selectZone
+    sendCmd "$brightDim\00"
 }
 function sendCoolWarmCommand {
-	coolWarm=$1
-	selectZone
-	sendCmd "$coolWarm\00"
+    coolWarm=$1
+    selectZone
+    sendCmd "$coolWarm\00"
 }
 
 ##########
 # Input Handling Functions
 ##########
 function handleOn {
-	echo "You just turned white bulbs in zone $zone on"
-	sendOnCommand	
+    echo "You just turned white bulbs in zone $zone on"
+    sendOnCommand    
 }
 function handleOff {
-	echo "You just turned white bulbs in zone $zone off"
-	sendOffCommand	
+    echo "You just turned white bulbs in zone $zone off"
+    sendOffCommand    
 }
 function handleBrightness {
     case $param in
@@ -105,12 +105,12 @@ function handleInteractive {
     echo "Make sure you have numlock ON when using numpad"
     for (( ; ; ))
     do
-		read -s -n 1 var
+        read -s -n 1 var
         case $var in
         8)
             echo "You turned white bulbs in zone $zone up 1 brightness"
             sendBrightDimCommand "\x3C";;
-		2)
+        2)
             echo "You turned white bulbs in zone $zone down 1 brightness"
             sendBrightDimCommand "\x34";;
         4)
@@ -121,8 +121,8 @@ function handleInteractive {
             sendCoolWarmCommand "\x3e";;
         *)
             echo "wrong key pressed"
-		esac
-	done
+        esac
+    done
 }
 
 ##########
